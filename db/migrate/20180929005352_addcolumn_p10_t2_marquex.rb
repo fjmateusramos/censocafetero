23386d5935880c6ff4f97bb1acc7fa1299c7fa8f
fjmateusramos <fjmateusramos@gmail.com>
2018-10-01 05:19:07 -0500

    saltos por pagina modulo 6

---
+++ db/migrate/20180929005352_addcolumn_p10_t2_marquex.rb
@@ -0,0 +1,51 @@
+class AddcolumnP10T2Marquex < ActiveRecord::Migration[5.2]
+  def change
+
+
+
+add_column :encuesta, :p10_t2_a1x, :string
+
+add_column :encuesta, :p10_t2_a2x, :string
+
+add_column :encuesta, :p10_t2_a3x, :string
+
+add_column :encuesta, :p10_t2_b1x, :string
+
+add_column :encuesta, :p10_t2_b2x, :string
+
+add_column :encuesta, :p10_t2_c1x, :string
+
+add_column :encuesta, :p10_t2_c2x, :string
+
+add_column :encuesta, :p10_t2_d1x, :string
+
+add_column :encuesta, :p10_t2_d2x, :string
+
+add_column :encuesta, :p10_t2_d3x, :string
+
+add_column :encuesta, :p10_t2_d4x, :string
+
+add_column :encuesta, :p10_t2_d5x, :string
+
+add_column :encuesta, :p10_t2_d6x, :string
+
+add_column :encuesta, :p10_t2_e1x, :string
+
+add_column :encuesta, :p10_t2_e2x, :string
+
+add_column :encuesta, :p10_t2_e3x, :string
+
+
+add_column :encuesta, :p10_t2_f1x, :string
+
+add_column :encuesta, :p10_t2_f2x, :string
+
+add_column :encuesta, :p10_t2_gx, :string
+
+add_column :encuesta, :p10_t2_hx, :string
+
+
+
+
+  end
+end
