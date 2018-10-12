816724b463722e09729390cd14be201e57ac371a
fjmateusramos <fjmateusramos@gmail.com>
2018-10-12 01:10:27 -0500

    saltos por pagina modulo 7

--- app/helpers/encuesta_helper.rb
+++ app/helpers/encuesta_helper.rb
@@ -11,6 +11,23 @@ module EncuestaHelper
   		@encuestum.p4_38_6.nil? ? false : @encuestum.p4_38_6.match(area1)
   		@encuestum.p4_38_7.nil? ? false : @encuestum.p4_38_7.match(area1)
   		@encuestum.p4_38_8.nil? ? false : @encuestum.p4_38_8.match(area1)
+      @encuestum.p4_38_8.nil? ? false : @encuestum.p4_38_8.match(area1)
+      @encuestum.p7_65_1.nil? ? false : @encuestum.p7_65_1.match(area1)
+      @encuestum.p7_68_1.nil? ? false : @encuestum.p7_68_1.match(area1)
+      @encuestum.p7_65_2.nil? ? false : @encuestum.p7_65_2.match(area1)
+      @encuestum.p7_68_2.nil? ? false : @encuestum.p7_68_2.match(area1)
+      @encuestum.p7_65_3.nil? ? false : @encuestum.p7_65_3.match(area1)
+      @encuestum.p7_68_3.nil? ? false : @encuestum.p7_68_3.match(area1)
+      @encuestum.p7_65_4.nil? ? false : @encuestum.p7_65_4.match(area1)
+      @encuestum.p7_68_4.nil? ? false : @encuestum.p7_68_4.match(area1)
+      @encuestum.p7_65_5.nil? ? false : @encuestum.p7_65_5.match(area1)
+      @encuestum.p7_68_5.nil? ? false : @encuestum.p7_68_5.match(area1)
+      @encuestum.p7_65_6.nil? ? false : @encuestum.p7_65_6.match(area1)
+      @encuestum.p7_68_6.nil? ? false : @encuestum.p7_68_6.match(area1)
+      @encuestum.p7_65_7.nil? ? false : @encuestum.p7_65_7.match(area1)
+      @encuestum.p7_68_7.nil? ? false : @encuestum.p7_68_7.match(area1)
+      @encuestum.p7_65_8.nil? ? false : @encuestum.p7_65_8.match(area1)
+      @encuestum.p7_68_8.nil? ? false : @encuestum.p7_68_8.match(area1)
 	end
 
 
