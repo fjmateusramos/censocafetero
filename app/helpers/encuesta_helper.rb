4521f9638d8a0c21edc237f117682eaa49293a59
fjmateusramos <fjmateusramos@gmail.com>
2018-10-11 21:13:43 -0500

    saltos por pagina modulo 2

--- app/helpers/encuesta_helper.rb
+++ app/helpers/encuesta_helper.rb
@@ -1,2 +1,6 @@
 module EncuestaHelper
+	def checked(area)
+  		@encuestum.p2_25.nil? ? false : @encuestum.p2_25.match(area)
+  		@encuestum.p2_26.nil? ? false : @encuestum.p2_26.match(area)
+	end
 end
--- app/models/encuestum.rb
+++ app/models/encuestum.rb
@@ -1,3 +1,8 @@
 class Encuestum < ApplicationRecord
 	has_many :encuesta1s, dependent: :destroy
+
+	before_save do
+	  self.p2_25.gsub!(/[\[\]\"]/, "") if attribute_present?("p2_25")
+	  self.p2_26.gsub!(/[\[\]\"]/, "") if attribute_present?("p2_26")
+	end
 end
