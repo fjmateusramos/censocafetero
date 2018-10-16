11a5b751f03894a612d41d0bf22d3d21349c7e9d
fjmateusramos <fjmateusramos@gmail.com>
2018-10-16 05:53:47 -0500

    saltos por pagina modulo 11

--- Gemfile
+++ Gemfile
@@ -25,6 +25,7 @@ gem 'jbuilder', '~> 2.5'
 gem 'bootstrap', '~> 4.1.3'
 gem 'sprockets-rails'
 gem 'jquery-rails'
+gem 'rack-offline'
 # Use Redis adapter to run Action Cable in production
 # gem 'redis', '~> 4.0'
 # Use ActiveModel has_secure_password
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
+  
   resources :encuesta1s
   resources :encuesta
   root 'encuesta#new'
+  get "/application.manifest" => Rails::Offline
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
